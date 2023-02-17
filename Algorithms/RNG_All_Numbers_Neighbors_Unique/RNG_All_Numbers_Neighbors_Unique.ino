eecc7388b1a661a45380a74f62ff176448615370
Adam Billingsley <laplacierece@gmail.com>
2023-02-17 09:18:47 -0500

    RNG_Numbers_Neighbors_Unique working, Switch_Light_Array_RNG in progress

--- Algorithms/RNG_All_Numbers_Neighbors_Unique/RNG_All_Numbers_Neighbors_Unique.ino
+++ Algorithms/RNG_All_Numbers_Neighbors_Unique/RNG_All_Numbers_Neighbors_Unique.ino
@@ -42,15 +42,15 @@
 #define uniqueNum     11 // Number of unique numbers from 0 to uniqueNum-1
 #define patternLength 20 // Length of generated pattern
 
-byte elimArr[uniqueNum]; // Stores the pattern data for an elimination sort
-byte elimLower;          // Lower bound for elimination sort
-byte elimUpper;          // Upper bound for elimination sort
+byte resArr[uniqueNum]; // Stores the pattern data for an resevoir sampling
+byte resLower;          // Lower bound for resevoir sampling
+byte resUpper;          // Upper bound for resevoir sampling
 byte numToGo;            // Number of numbers left to generate for pattern
 byte solution[patternLength]; // Stores valid pattern
 
 /*
  * Function: shiftOutLeft
- * This function accepts an elimination sort array, the position to start 
+ * This function accepts a resevoir sampling array, the position to start 
  * shifting, and the upper bound of the shift. Shifts the array left from 
  * these two bounds. Does not resize the input array to increase speed; 
  * user must handle ignoring the unnecessary values.
@@ -72,15 +72,15 @@ byte solution[patternLength]; // Stores valid pattern
  *  - Decrement Upper bound by 1 and exit.
  */
 void shiftOutLeft(byte pos){
-  for(int i=pos; i<elimUpper; i++){ // Starting at index=pos, for each index of the array to the upper bound...
-    elimArr[i] = elimArr[i+1];      // Shift index on the right to the current index.
+  for(int i=pos; i<resUpper; i++){ // Starting at index=pos, for each index of the array to the upper bound...
+    resArr[i] = resArr[i+1];      // Shift index on the right to the current index.
   }
-  elimUpper--;                      // Decrement upper bound by 1 to ignore useless data
+  resUpper--;                      // Decrement upper bound by 1 to ignore useless data
 }
 
 /*
  * Function: shiftOutRight
- * This function accepts an elimination sort array, the position to finish 
+ * This function accepts a resevoir sampling array, the position to finish 
  * shifting, and the lower bound of the shift. Shifts the array right from 
  * these two bounds. Does not resize the input array to increase speed; 
  * user must handle ignoring the unnecessary values.
@@ -102,10 +102,10 @@ void shiftOutLeft(byte pos){
  *  - Increment Lower bound by 1 and exit.
  */
 void shiftOutRight(byte pos){
-  for(int i=pos-1; i>=elimLower; i--){ // Starting at index=pos-1, for each index of the array to elimLower...
-    elimArr[i+1] = elimArr[i];         // Shift index on the current index to the right.
+  for(int i=pos-1; i>=resLower; i--){ // Starting at index=pos-1, for each index of the array to resLower...
+    resArr[i+1] = resArr[i];         // Shift index on the current index to the right.
   }
-  elimLower++;                         // Increment lower bound by 1 to ignore useless data
+  resLower++;                         // Increment lower bound by 1 to ignore useless data
 }
 
 /*
@@ -146,49 +146,49 @@ void shiftInRight(byte insert, byte pos, byte limit = patternLength-numToGo){
 }
 
 /*
- * Function: elimSort
- * This function accepts an elimination sort array, a lower bound, and an
- * upper bound. Generates a random number between the lower and upper bound and performs a single elimination sort. Returns a post processed
- * elimination sort array and new lower and upper bounds.
+ * Function: resSort
+ * This function accepts a resevoir sampling array, a lower bound, and an
+ * upper bound. Generates a random number between the lower and upper bound and performs a single resevoir sample. Returns a post processed
+ * resevoir sampling array and new lower and upper bounds.
  * 
- * An elimination sort is effective for speed when:
+ * Resevoir sampling is effective for speed when:
  *  - Attempting to find a unique number.
  *  - The number of unique values is large.
  * 
  * In a traditional loop to check each previous number, we can be checking up 
- * to n^2 numbers for each number, with no guarantee of a unique value. In an 
- * elimination sort, no matter how many unique numbers there are, a generation 
- * of a unique number in an elimination sort is a single array index check with 
+ * to n^2 numbers for each number, with no guarantee of a unique value. In 
+ * resevoir sampling, no matter how many unique numbers there are, a generation 
+ * of a unique number in resevoir sampling is a single array index check with 
  * a guaranteed unique value.
  * 
  * Time complexity of:
  *   - Traditional sort - O(n^2)
- *   - Elimination sort - O(n + n/2) = O(n)
+ *   - Resevoir sampling - O(n + n/2) = O(n)
  * Space complexity of:
  *   - Traditional sort - O(n)
- *   - Elimination sort - O(2n) = O(n)
+ *   - Resevoir sampling - O(2n) = O(n)
  * 
- * However, an elimination sort, by nature, does not create duplicate numbers. 
+ * However, resevoir sampling, by nature, does not create duplicate numbers. 
  * This method cannot be used for generating an amount of numbers greater than 
  * the number of unique numbers.
  * 
  * In our all numbers neighbors unique example, this method is used whenever 
  * it is applicable due to its speed.
  */
-byte elimSort(){
-  byte pos = random(elimLower, elimUpper+1);              // Randomly select an index of a number to pull for an elimination sort
-  byte num = elimArr[pos];                                // Store that number
-  if(elimArr[pos - elimLower] < elimArr[elimUpper - pos]) // If that number is closer to the left side of the elimination array...
-    shiftOutRight(pos);                                   // Shift elimination array right
+byte resSort(){
+  byte pos = random(resLower, resUpper+1);              // Randomly select an index of a number to pull for resevoir sampling
+  byte num = resArr[pos];                                // Store that number
+  if(resArr[pos - resLower] < resArr[resUpper - pos]) // If that number is closer to the left side of the resevoir sampling array...
+    shiftOutRight(pos);                                   // Shift resevoir sampling array right
   else 
-    shiftOutLeft(pos);                                    // Otherwise, shift elimination array left
-  return num;                                             // Return the number shifted out of the elimination array
+    shiftOutLeft(pos);                                    // Otherwise, shift resevoir sampling array left
+  return num;                                             // Return the number shifted out of the resevoir sampling array
 }
 
 /*
  * Function: generatePattern 
  * This function generates a random all numbers, neighbors unique pattern.
- * It utilizes elimination sorting to ensure all unique numbers are present
+ * It utilizes resevoir sampling to ensure all unique numbers are present
  * in the pattern. Afterwards, it generates random numbers from 0 to 
  * uniqueNum-1 and attempts to insert them into a random location in the
  * pattern, following the unique neighbor rule. It does this until the
@@ -196,40 +196,40 @@ byte elimSort(){
  */
 
 void generatePattern(){
-  elimUpper = uniqueNum-1;                                            // Initialize upper bound of elimination sort
-  elimLower = 0;                                                      // Initialize lower bound of elimination sort
-  numToGo = patternLength;                                            // Stores number of numbers left to generate in pattern
-  for(byte i=0; i<uniqueNum; i++)                                     // For each index in the elimination sort array...
-    elimArr[i] = i;                                                   // Initialize the array with every unique number
+  resUpper = uniqueNum-1;          // Initialize upper bound of resevoir sampling
+  resLower = 0;                    // Initialize lower bound of resevoir sampling
+  numToGo = patternLength;         // Stores number of numbers left to generate in pattern
+  for(byte i=0; i<uniqueNum; i++)  // For each index in the resevoir sampling array...
+    resArr[i] = i;                 // Initialize the array with every unique number
     
-  for(byte i=0; i<uniqueNum; i++){                                    // For each unique number...
-    solution[i] = elimSort();                                         // Perform an elimination sort and store the selected number
-    numToGo--;                                                        // Denote one less number left to generate
+  for(byte i=0; i<uniqueNum; i++){ // For each unique number...
+    solution[i] = resSort();       // Perform resevoir sampling and store the selected number
+    numToGo--;                     // Denote one less number left to generate
   }
-  while(numToGo){                                                     // If we still have numbers to generate...
-    byte randNum = random(0, uniqueNum);                              // Generate a random number from 0 to uniqueNum - 1
-    byte pos = random(0, patternLength - numToGo);                    // Generate a random position within the bounds of generated numbers
-    bool flag_inserted = false;                                       // Flag checks if number has been inserted into pattern
+  while(numToGo){                                                           // If we still have numbers to generate...
+    byte randNum = random(0, uniqueNum);                                    // Generate a random number from 0 to uniqueNum - 1
+    byte pos = random(0, patternLength - numToGo);                          // Generate a random position within the bounds of generated numbers
+    bool flag_inserted = false;                                             // Flag checks if number has been inserted into pattern
 
-    while(!flag_inserted){                                                // While the number has not been inserted into the pattern...
+    while(!flag_inserted){                                                  // While the number has not been inserted into the pattern...
       //NICHE CASES
-      if(pos == patternLength - numToGo - 1 && randNum != solution[pos]){ // If the new position is at the final generated number and is a valid insertion...
-        shiftInRight(randNum, pos+1, pos+1);                              // Insert number at the end
+      if(pos == patternLength - numToGo - 1 && randNum != solution[pos]){   // If the new position is at the final generated number and is a valid insertion...
+        shiftInRight(randNum, pos+1, pos+1);                                // Insert number at the end
         flag_inserted = true;
       }
       else if(pos > patternLength - numToGo - 1 && randNum != solution[0]){ // If the new position is outside the bounds of generated numbers and can be inserted at the beginning...
         shiftInRight(randNum, 0);                                           // Insert number at beginning
         flag_inserted = true;
       }
-      else if(pos > patternLength - numToGo && randNum == solution[0]){ // Otherwise, if the new position is outside the bounds of generated numbers and the first number is an identical neighbor...
-        pos = 1;                                                        // Move the position to +1 after the beginning  
+      else if(pos > patternLength - numToGo && randNum == solution[0]){     // Otherwise, if the new position is outside the bounds of generated numbers and the first number is an identical neighbor...
+        pos = 1;                                                            // Move the position to +1 after the beginning  
       }
       //STANDARD CASES
-      else if(randNum == solution[pos] || randNum == solution[pos+1]){  // If number has an identical neighbor...
-        pos += 2;                                                       // Increment the position to check the next pair of neighbors
+      else if(randNum == solution[pos] || randNum == solution[pos+1]){      // If number has an identical neighbor...
+        pos += 2;                                                           // Increment the position to check the next pair of neighbors
       }
-      else{                                                             // If number has unique neighbors...
-        shiftInRight(randNum, pos+1);                                   // Insert the number between them
+      else{                                                                 // If number has unique neighbors...
+        shiftInRight(randNum, pos+1);                                       // Insert the number between them
         flag_inserted = true;
       }
     }
@@ -240,8 +240,8 @@ void generatePattern(){
 void setup() {
   Serial.begin(19200);
   randomSeed(analogRead(A0));     // Use noise fluxuations from the A0 pin to seed the RNG
-  for(byte i=0; i<uniqueNum; i++) // For each index in the elimination sort array...
-    elimArr[i] = i;               // Initialize the array with every unique number
+  for(byte i=0; i<uniqueNum; i++) // For each index in the resevoir sampling array...
+    resArr[i] = i;                // Initialize the array with every unique number
 
   generatePattern();
   Serial.println();
--- Puzzles/Switch_Light_Array_RNG/01_PuzzleFunc.ino
+++ Puzzles/Switch_Light_Array_RNG/01_PuzzleFunc.ino
@@ -3,21 +3,87 @@ static const char BIT_LOOKUP[16] = { // Lookup table for number of bits set in a
   1, 2, 2, 3, 2, 3, 3, 4
 };
 
+byte elimArr[uniqueNum];      // Stores the pattern data for an elimination sort
+byte elimLower;               // Lower bound for elimination sort
+byte elimUpper;               // Upper bound for elimination sort
+byte numToGo;                 // Number of numbers left to generate for pattern
+byte solution[patternLength]; // Stores valid pattern
+
 /* 
  * Function: getLightCount
  * Takes a light pattern and returns number of lights that are on.
  * Uses bit shifting with a static lookup table to speed this process up
- * in case of large amounts of lights.
+ * which helps tremendously with larger amounts of lights.
  */
 byte getLightCount(int lights){
   byte count = 0;
   while(!lights){ // While ON lights still exist...
     count += BIT_LOOKUP[lights & 0x0F]; // Grab number of ON lights in current 4 lights
-    lights >>= 4; // Change focus to the next 4 lights
+    lights >>= 4; // Remove those 4 lights and move to the next 4
   }
   return count; 
 }
- 
+
+ /*
+ * Function: shiftOutRight
+ * This function accepts an elimination sort array, the position to finish 
+ * shifting, and the lower bound of the shift. Shifts the array right from 
+ * these two bounds. Does not resize the input array to increase speed; 
+ * user must handle ignoring the unnecessary values.
+ * 
+ * Example: Lower bound = 0, position = 4
+ * 
+ *  - Start at i = (position - 1) = 3
+ *  - Copy value at i to (i + 1)
+ *  Index: 0  1  2  3  4  5  6
+ *  Value: 1  3  5 [7]>9  0  8
+ * 
+ *  Index: 0  1  2  3  4  5  6
+ *  Value: 1  3  5  7  7  0  8
+ *  
+ *  - Repeat until i < Lower bound, i = -1
+ *  Index: 0  1  2  3  4  5  6
+ *  Value: 1  1  3  5  7  0  8
+ *  
+ *  - Increment Lower bound by 1 and exit.
+ */
+void shiftOutRight(byte pos){
+  for(int i=pos-1; i>=elimLower; i--){ // Starting at index=pos-1, for each index of the array to elimLower...
+    elimArr[i+1] = elimArr[i];         // Shift index on the current index to the right.
+  }
+  elimLower++;                         // Increment lower bound by 1 to ignore useless data
+}
+
+/*
+ * Function: shiftOutLeft
+ * This function accepts an elimination sort array, the position to start 
+ * shifting, and the upper bound of the shift. Shifts the array left from 
+ * these two bounds. Does not resize the input array to increase speed; 
+ * user must handle ignoring the unnecessary values.
+ * 
+ * Example: Upper bound = 6, position = 2
+ * 
+ *  - Start at i = position = 2
+ *  - Copy value at (i + 1) to i
+ *  Index: 0  1  2  3  4  5  6
+ *  Value: 1  3 [5]<7  9  0  8
+ * 
+ *  Index: 0  1  2  3  4  5  6
+ *  Value: 1  3  7  7  9  0  8
+ *  
+ *  - Repeat until i >= Upper bound, i = 6
+ *  Index: 0  1  2  3  4  5  6
+ *  Value: 1  3  7  9  0  8  8
+ *  
+ *  - Decrement Upper bound by 1 and exit.
+ */
+void shiftOutLeft(byte pos){
+  for(int i=pos; i<elimUpper; i++){ // Starting at index=pos, for each index of the array to the upper bound...
+    elimArr[i] = elimArr[i+1];      // Shift index on the right to the current index.
+  }
+  elimUpper--;                      // Decrement upper bound by 1 to ignore useless data
+}
+
 /* 
  * Function: generateSolution
  * Takes the globally defined number of switches and number of
@@ -27,19 +93,19 @@ byte getLightCount(int lights){
 void generateSolution(){
   int lightCombos = (2^numLight) - 1; // Total number of possible light combinations
   bool solutionSwitches[numSolution]; // Array of correct switches to turn on
-  int solutionLights[numSolution]; // Array of light patterns for correct switches
-  bool countSwitches[numSwitch]; // Count array to track switch occurences in solution
-  bool countLights[lightCombos]; // Count array to track light pattern occurences in solution
-  for (int i=0; i<numSwitch; i++){ // For each possible switch position...
-    countSwitches[i] = 0; // Initialize occurences to 0
+  int solutionLights[numSolution];    // Array of light patterns for correct switches
+  byte elimSwitches[numSwitch];       // Elimination sort array to track remaining switch indexes
+  bool countLights[lightCombos];      // Count array to track light pattern occurences in solution
+  for (int i=0; i<numSwitch; i++){    // For each possible switch...
+    elimSwitches[i] = i;              // Initialize elimination sort for selecting correct switches
   }
-  for(int i=0; i<lightCombos; i++){ // For each possible light pattern...
-    countLights[i] = 0; // Initialize occurences to 0
+  for(int i=0; i<lightCombos; i++){   // For each possible light pattern...
+    countLights[i] = 0;               // Initialize occurences to 0
   }
-  for (int i=0; i<numSolution; i++){ // For each switch and light pattern solution...
+  for (int i=0; i<numSolution; i++){  // For each switch and light pattern solution...
     do {
       solutionSwitches[i] = random(0,numSwitch); // Generate a random index of a switch
-    }while(countSwitches[solutionSwitches[i]]); // If the switch already exists in solution...
+    }while(countSwitches[solutionSwitches[i]]);  // If the switch already exists in solution...
     solutionLights[i] = random(1,lightCombos-1); // Generate a light pattern, but not all OFF or ON
     do {
       solutionLights[i] = random(1,lightCombos-1); // Generate another light pattern, but not all OFF or ON
