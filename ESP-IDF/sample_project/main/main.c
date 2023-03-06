2487f5fa1e1035c0083a3298fd36b45934f4be85
Adam Billingsley <ABillingsley@horrycountyschools.net>
2023-03-06 15:32:39 -0500

    more work on esp32 can template. self tests working.

--- ESP-IDF/sample_project/main/main.c
+++ ESP-IDF/sample_project/main/main.c
@@ -13,8 +13,8 @@
 
 /* --------------------- Definitions and static variables ------------------ */
 // GPIO Pin assignments
-#define TX_GPIO_NUM                     19 // ESP32 Tx Pin to CAN Pin
-#define RX_GPIO_NUM                     22 // ESP32 Rx Pin to CAN Pin
+#define TX_GPIO_NUM                     17 // ESP32 Tx Pin to CAN Pin
+#define RX_GPIO_NUM                     16 // ESP32 Rx Pin to CAN Pin
 
 // FreeRTOS task priorities
 #define CTRL_TASK_PRIO                  10 // Control_Task
@@ -28,94 +28,177 @@
 
 #define ID_PROP                         0x01 // 8-bit Prop ID, combined with 3-bit priority for full CAN ID
 static const twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
-static const twai_timing_config_t t_config = TWAI_TIMING_CONFIG_25KBITS();
+static const twai_timing_config_t t_config = TWAI_TIMING_CONFIG_125KBITS();
 static const twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(TX_GPIO_NUM, RX_GPIO_NUM, TWAI_MODE_NO_ACK);
-static const twai_message_t tx_msg = {.identifier = 0, .data_length_code = 0};
-static const twai_message_t ping_resp = {.identifier = 0b01100000000 + ID_PROP, .data_length_code = 0,
-                                        .data = {0,0,0,0,0,0,0,0}};
+static const twai_message_t ping_resp = {.identifier = 0b11000000000 + ID_PROP, .data_length_code = 0,
+                                        .data = {0,0,0,0,0,0,0,0}, .self = 1};
+static const twai_message_t ping_req = {.identifier = 0b11100000000 + 0x00, .data_length_code = 1,
+                                        .data = {1,0,0,0,0,0,0,0}, .self = 1};
+static QueueHandle_t ctrl_task_queue;
+static QueueHandle_t tx_task_queue;
 static SemaphoreHandle_t ctrl_task_sem;
 static SemaphoreHandle_t rx_task_sem;
 static SemaphoreHandle_t tx_task_sem;
 //static SemaphoreHandle_t puzzle_task_sem;
 //static SemaphoreHandle_t music_task_sem;
 //static SemaphoreHandle_t gpio_task_sem;
+static SemaphoreHandle_t state_set_mutex;
+
+uint8_t[7] rx_payload;
 
 typedef enum {
     BEGIN,
-    TX_HELLO,
-    TX_PING,
     RX_PING,
-    TX_DATA,
-    SOLVE,
-    RESET,
+    STATE_SET,
+    GPIO_SET,
     PLAY_SOUND,
     INHERIT_REQ,
-    INHERIT_PASS,
     INHERIT_STOP
 } ctrl_task_action_t;
-
-typedef enum {
-    RX_PING,
-    SOLVE,
-    RESET,
-    PLAY_SOUND,
-    INHERIT_STOP
-} rx_task_action_t;
 
 typedef enum {
     TX_HELLO,
     TX_PING,
     TX_DATA,
     TX_DND,
-    TX_INHERIT
+    TX_INHERIT,
+    INHERIT_PASS
 } tx_task_action_t;
 
 
 static void ctrl_task(void *arg){
+    ctrl_task_action_t ctrl_action;
+    //puzzle_task_action_t puzzle_action;
+    tx_task_action_t tx_action;
     static const char* TAG = "CAN_Controller";
+    xSemaphoreGive(rx_task_sem); // Allow rx_task to begin receiving CAN messages
     for(;;){
         xSemaphoreTake(ctrl_task_sem, portMAX_DELAY); // Blocked from executing until app_main, puzzle_task or rx_task gives a semaphore
-
+        xQueueReceive(ctrl_task_queue, &ctrl_action, pdMS_TO_TICKS(10)); // Pull task from queue
+        if(BEGIN){
+            tx_action = TX_PING;
+            xQueueSend(tx_task_queue, &tx_action, portMAX_DELAY);
+            xSemaphoreGive(tx_task_sem);
+            ESP_LOGI(TAG, "Init successful");
+        }
+        else if(RX_PING){
+            tx_action = TX_PING;
+            xQueueSend(tx_task_queue, &tx_action, portMAX_DELAY);
+            xSemaphoreGive(tx_task_sem);
+            ESP_LOGI(TAG, "Sent ping task to TX");
+        }
+        else if(STATE_SET){
+            //puzzle_action = SET_STATE
+            //xQueueSend(puzzle_task_queue, &puzzle_action, portMAX_DELAY);
+            //xSemaphoreGive(puzzle_task_sem);
+            ESP_LOGI(TAG, "Sent state %d to TX",rx_set_state);
+        }
     }
 }
 
 static void rx_task(void *arg){
     static const char* TAG = "CAN_Rx";
+    static const char* type[8]= {"ALL_COMMAND","COMMAND","unused","unused","INHERIT","ALL_PING_REQ","PING_RESP","PING_REQ"};
+    static const char* cmd[3]= {"STATE","GPIO","MUSIC"};
+    uint8_t msg_id;
+    uint8_t msg_type;
+    uint8_t msg_cmd;
+    twai_message_t rx_msg;
+    ctrl_task_action_t ctrl_action;
     xSemaphoreTake(rx_task_sem, portMAX_DELAY); // Blocked from beginning until ctrl_task gives semaphore
-    ESP_LOGD(TAG, "Task initialized");
-    for(;;){ // Runs forever after taking semaphore
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Simulating no messages
+    ESP_LOGI(TAG, "Task initialized");
+    while(1){ // Runs forever after taking semaphore
+        //Wait for message
+        if (twai_receive(&rx_msg, pdMS_TO_TICKS(11000)) == ESP_OK) { // Wait for messages on CAN bus
+            ESP_LOGI(TAG, "Received message...");
+            msg_id = (uint8_t)(rx_msg.identifier & 0xFF);
+            msg_type = (uint8_t)(rx_msg.identifier >> 8);
+            ESP_LOGI(TAG, "From_ID: %d, Type: %s, To_ID: %d",msg_id,type[msg_type],rx_msg.data[0]);
+            switch(msg_type){
+                case 0: case 1: // Command
+                    msg_cmd = rx_msg.data[1];
+                    ESP_LOGI(TAG, "Command: %s",cmd[msg_cmd]);
+                    ctrl_action = RX_CMD;
+                    for(int i=1; i<rx_msg.data_length_code; i++){
+                        rx_payload[i-1] = rx_msg.data[i];
+                    }
+                    xQueueSend(ctrl_task_queue, &ctrl_action, portMAX_DELAY);
+                    xSemaphoreGive(ctrl_task_sem);
+                    break;
+                case 5: case 7: // Ping request
+                    ctrl_action = RX_PING;
+                    xQueueSend(ctrl_task_queue, &ctrl_action, portMAX_DELAY);
+                    xSemaphoreGive(ctrl_task_sem);
+                    break;
+                case 6: // Ping response
+                    break;
+                default: // Unknown command
+            }
+        } else {
+            ESP_LOGI(TAG, "No messages on the bus?");
+        }
     }
 }
 
 static void tx_task(void *arg){
     tx_task_action_t action;
     static const char* TAG = "CAN_Tx";
-    ESP_LOGD(TAG, "Task initialized");
+    ESP_LOGI(TAG, "Task initialized");
     for(;;){
         xSemaphoreTake(tx_task_sem, portMAX_DELAY); // Blocked from executing until ctrl_task gives semaphore
-        xQueueReceive(tx_task_queue, &action, portMAX_DELAY);
-        if (action == TX_SEND_PING_RESP) {
-            //Transmit ping response to master
-            twai_transmit(&ping_resp, portMAX_DELAY);
-            ESP_LOGI(EXAMPLE_TAG, "Transmitted ping response");
+        xQueueReceive(tx_task_queue, &action, pdMS_TO_TICKS(10)); // Pull task from queue
+        if (action == TX_PING) {
+            //Queue message for transmission
+            if (twai_transmit(&ping_resp, portMAX_DELAY) == ESP_OK) {
+                ESP_LOGI(TAG, "Transmitted ping response");
+            } else {
+                ESP_LOGI(TAG, "Failed to transmit message");
+            }
         }
     }
 }
 
+static void fake_bus_task(void *arg){
+    tx_task_action_t action;
+    static const char* TAG = "CAN_Fake_Bus";
+    ESP_LOGI(TAG, "Task initialized");
+    for(;;){
+        //Fake ping request
+        if (twai_transmit(&ping_req, portMAX_DELAY) == ESP_OK) {
+            ESP_LOGI(TAG, "Transmitted ping request");
+        } else {
+            ESP_LOGI(TAG, "Failed to transmit request");
+        }
+        vTaskDelay(pdMS_TO_TICKS(5000)); // Simulating no messages
+    }
+}
+
 void app_main(void){
+    static const char* TAG = "Init_Main";
     tx_task_queue = xQueueCreate(1, sizeof(tx_task_action_t));
-    rx_task_queue = xQueueCreate(1, sizeof(rx_task_action_t));
     ctrl_task_queue = xQueueCreate(1, sizeof(ctrl_task_action_t));
     ctrl_task_sem = xSemaphoreCreateCounting( 10, 0 );
     rx_task_sem = xSemaphoreCreateBinary();
     tx_task_sem = xSemaphoreCreateCounting( 10, 0 );
     //puzzle_task_sem = xSemaphoreCreateCounting( 10, 0 );
+    rx_payload_sem = xSemaphoreCreateBinary(); // Allows rx_task to write to the data payload
+    puzzle_payload_sem = xSemaphoreCreateBinary(); // Allows puzzle_task to read the data payload
 
     xTaskCreatePinnedToCore(tx_task, "CAN_Tx", 4096, NULL, TX_TASK_PRIO, NULL, tskNO_AFFINITY);
     xTaskCreatePinnedToCore(ctrl_task, "CAN_Controller", 4096, NULL, CTRL_TASK_PRIO, NULL, tskNO_AFFINITY);
     xTaskCreatePinnedToCore(rx_task, "CAN_Rx", 4096, NULL, RX_TASK_PRIO, NULL, tskNO_AFFINITY);
+    xTaskCreatePinnedToCore(fake_bus_task, "CAN_Fake_Bus_Task", 4096, NULL, RX_TASK_PRIO, NULL, tskNO_AFFINITY);
     //xTaskCreatePinnedToCore(puzzle_task, "Puzzle", 4096, NULL, PUZZLE_TASK_PRIO, NULL, tskNO_AFFINITY);
     //xTaskCreatePinnedToCore(music_task, "Music", 4096, NULL, GENERIC_TASK_PRIO, NULL, tskNO_AFFINITY);
     //xTaskCreatePinnedToCore(gpio_task, "GPIO", 4096, NULL, GENERIC_TASK_PRIO, NULL, tskNO_AFFINITY);
+
+    //Install TWAI driver, trigger tasks to start
+    ESP_ERROR_CHECK(twai_driver_install(&g_config, &t_config, &f_config));
+    ESP_LOGI(TAG, "Driver installed");
+    ESP_ERROR_CHECK(twai_start());
+    ESP_LOGI(TAG, "Driver started");
+
+    ctrl_task_action_t ctrl_action = BEGIN;
+    xQueueSend(ctrl_task_queue, &ctrl_action, portMAX_DELAY);
+    xSemaphoreGive(ctrl_task_sem);              //Start Control task
 }
